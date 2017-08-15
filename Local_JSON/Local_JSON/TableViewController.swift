5a09ae6cef8d53fe3c669c424754080fb0934455
QKKQQK <zhouqiankang@hotmail.com>
2017-08-15 09:41:40 -0500

    Update Main storyboard to tableview

---
+++ Local_JSON/Local_JSON/TableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TableViewController.swift
+//  Local_JSON
+//
+//  Created by Qiankang Zhou on 8/15/17.
+//  Copyright © 2017 Qiankang Zhou. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
