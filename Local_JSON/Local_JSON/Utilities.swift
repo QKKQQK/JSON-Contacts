5d3d130995f3a798e25903ce97a50b5b462aa5db
QKKQQK <zhouqiankang@hotmail.com>
2017-08-15 09:35:11 -0500

    Copy given Utilities file to project

---
+++ Local_JSON/Local_JSON/Utilities.swift
@@ -0,0 +1,33 @@
+//
+//  Utilities.swift
+//  FirstLaunch
+//
+//  Created by Eric J Knapp on 8/7/17.
+//  Copyright © 2017 Eric J Knapp. All rights reserved.
+//
+
+import Foundation
+
+func isFirstLaunch() -> Bool {
+    let defaults = UserDefaults.standard
+    if defaults.bool(forKey: "FirstLaunch") == true {
+        print("First Launch!")
+        defaults.set(false, forKey: "FirstLaunch")
+        return true
+    } else {
+        print("Not first launch")
+        return false
+    }
+}
+
+func applicationDirectory() -> NSURL {
+    let fileManager = FileManager.default
+    
+    let urls = fileManager.urls(
+        for: FileManager.SearchPathDirectory.documentDirectory,
+        in: FileManager.SearchPathDomainMask.userDomainMask) as [NSURL]
+    
+    let documentsURL = urls[0]
+    
+    return documentsURL
+}
