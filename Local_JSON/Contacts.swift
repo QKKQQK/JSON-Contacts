d8e4198dfc8c36bb4fcba7c0b5761344ca9be7c2
QKKQQK <zhouqiankang@hotmail.com>
2017-08-20 13:32:48 -0500

    Move plist file create to new class ContactAll and modify ContactSection class

--- Local_JSON/Contacts.swift
+++ Local_JSON/Contacts.swift
@@ -47,83 +47,21 @@ struct Contact: Codable, CustomStringConvertible {
     }
 }
 
-class Contacts {
-    var contacts: [Contact]
+class ContactSection {
+    var section: [Contact]
     
     init() {
-        contacts = []
-        loadContacts()
+        section = []
     }
     
     var count: Int {
-        return contacts.count
+        return section.count
     }
     
-    func contact(at indexPath: IndexPath) -> Contact {
-        if indexPath.row >= contacts.count {
-            return contacts[0]
+    func getContactWithinSection(indexPath: IndexPath) -> Contact {
+        if indexPath.row >= section.count {
+            return section[0]
         }
-        
-        return contacts[indexPath.row]
-    }
-    
-    func loadContacts() {
-        if isFirstLaunch() {
-            print("Loading bundled JSON.")
-            contacts = loadBundledJSON()
-            createContactsPlistFile()
-        } else {
-            print("Loading contacts from app directory.")
-            contacts = loadSavedContacts()
-        }
-    }
-    
-    func loadBundledJSON() -> [Contact] {
-        let url = Bundle.main.url(forResource: "small-contacts", withExtension: "json")
-        
-        guard let jsonData = try? Data(contentsOf: url!) else {
-            return []
-        }
-        
-        let jsonDecoder = JSONDecoder()
-        
-        let contactsArray = try? jsonDecoder.decode([Contact].self, from: jsonData)
-        
-        return contactsArray ?? []
-    }
-    
-    func loadSavedContacts() -> [Contact] {
-        guard let contactsURL = applicationDirectory().appendingPathComponent("contacts.plist") else {
-            print("Can't create URl to app dir.")
-            return []
-        }
-        
-        guard let contactsData = try? Data.init(contentsOf: contactsURL) else {
-            print("Couldn't load data from app dir.")
-            return []
-        }
-        
-        let contactsDecoder = PropertyListDecoder()
-        let contactsArray = try? contactsDecoder.decode([Contact].self, from: contactsData)
-        
-        return contactsArray ?? []
-    }
-    
-    func createContactsPlistFile() {
-        guard let contactsPlistFileURL = applicationDirectory().appendingPathComponent("contacts.plist") else {
-            print("Could not get the URL for the data save.")
-            return
-        }
-        
-        let plistEncoder = PropertyListEncoder()
-        
-        // nil if fail, contacts.self stands for type
-        let contactsData = try? plistEncoder.encode(contacts.self)
-        
-        do {
-            try contactsData?.write(to: contactsPlistFileURL)
-        } catch {
-            print(error)
-        }
+        return section[indexPath.row]
     }
 }
